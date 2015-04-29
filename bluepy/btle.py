59e69a8f8a4f7822feea1877e4f9ab497a61238d
Clark Li <clark.li@minelab.com.au>
2015-04-29 15:43:00 +0930

    Unregister delegate on disconnection

--- bluepy/btle.py
+++ bluepy/btle.py
@@ -260,7 +260,8 @@ class Peripheral:
             if respType == 'ntfy':
                 hnd = resp['hnd'][0]
                 data = resp['d'][0]
-                self.delegate.handleNotification(hnd, data)
+                if self.delegate is not None:
+                    self.delegate.handleNotification(hnd, data)
                 if wantType != respType:
                     continue
 
@@ -298,6 +299,9 @@ class Peripheral:
     def disconnect(self):
         if self._helper is None:
             return
+        # Unregister the delegate first
+        self.setDelegate(None)
+
         self._writeCmd("disc\n")
         self._getResp('stat')
         self._stopHelper()
