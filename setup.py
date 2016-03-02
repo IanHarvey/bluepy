c535777a773a4a78c31f601182fb3b4d24e37636
Ian Harvey <website-contact@fenditton.org>
2016-03-02 09:47:05 +0000

    Update version to 1.0.3

--- setup.py
+++ setup.py
@@ -47,12 +47,12 @@ class BluepyDevelop(develop):
 
 setup (
     name='bluepy',
-    version='1.0.2',
+    version='1.0.3',
     description='Python module for interfacing with BLE devices through Bluez',
     author='Ian Harvey',
     author_email='website-contact@fenditton.org',
     url='https://github.com/IanHarvey/bluepy',
-    download_url='https://github.com/IanHarvey/bluepy/tarball/v/1.0.2',
+    download_url='https://github.com/IanHarvey/bluepy/tarball/v/1.0.3',
     keywords=[ 'Bluetooth', 'Bluetooth Smart', 'BLE', 'Bluetooth Low Energy' ],
     classifiers=[
         'Programming Language :: Python :: 2.7',
