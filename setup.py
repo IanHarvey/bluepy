bda163ddfee26d9164f456a203bbbe04f6467b1c
Maurice (mausy5043) Hendrix <2380344-mausy5043@users.noreply-gitlab.com>
2022-04-10 13:14:28 +0200

    add 3.9

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.9',
     ],
     packages=['bluepy'],
 
