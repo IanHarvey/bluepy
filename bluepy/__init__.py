a8a803507cb1c3e8107f6ed2efb45751ee231dba
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2023-02-21 17:35:02 +0100

    moved

--- bluepy/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-from . import btle
-from . import sensortag
-from . import thingy52
-
-__all__ = ["btle", "sensortag", "thingy52"]
--- bluepy/version.h
+++
@@ -1,1 +0,0 @@
-#define VERSION_STRING "1.3.0"
